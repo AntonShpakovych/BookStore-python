1be72969312cb8ee87a885ec4afe929c313bd2b7
Anton Shpakovych <shpakovych18@gmail.com>
2022-10-18 20:30:28 +0300

    'step2'

--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,4 @@
-"""Models"""
 from django.db import models
 
 class Category(models.Model):
-    """Model Category
-        name: string, max: 30
-    """
-
     name = models.CharField(max_length=30)
