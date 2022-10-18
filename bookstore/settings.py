1417652fa384db3ee25a7f499b667cbd94bffd3f
Anton Shpakovych <shpakovych18@gmail.com>
2022-10-18 20:09:32 +0300

    drop circleci

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from simple_cred.db_cred import PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bookstore',
         'USER': 'bookstore',
-        'PASSWORD': '45RB^b1V3xNq',
+        'PASSWORD': PASSWORD,
         'HOST': '',
         'PORT': '',
         'TEST': {
