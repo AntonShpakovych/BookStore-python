3b6e3d9c3600879ed76b15f2d053643fbd1b2182
Anton Shpakovych <shpakovych18@gmail.com>
2022-10-18 16:47:06 +0300

    'change_circle'

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-# from simple_cred.db_cred import PASSWORD
+from simple_cred.db_cred import PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bookstore',
         'USER': 'bookstore',
-        'PASSWORD': '45RB^b1V3xNq'
+        'PASSWORD': PASSWORD
     }
 }
 
