d8c251c4c294c8db83b5fea63542385e4b819fd2
Anton Shpakovych <shpakovych18@gmail.com>
2022-10-31 14:26:33 +0200

    change setting.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -1,10 +1,9 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 import django_heroku
 import dj_database_url
-from dotenv import dotenv_values
-
-config = dotenv_values(".env")
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
@@ -72,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bookstore',
         'USER': 'bookstore',
-        'PASSWORD': config['PASSWORD'],
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
