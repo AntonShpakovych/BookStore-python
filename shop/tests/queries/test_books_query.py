dd4c40e2c5bb01f09a5a50b15db28544751c135a
Anton Shpakovych <shpakovych18@gmail.com>
2022-11-01 13:35:42 +0200

    uncommenttest

--- shop/tests/queries/test_books_query.py
+++ shop/tests/queries/test_books_query.py
@@ -1,27 +1,27 @@
-# from django.test import TestCase
-# from shop.queries.books_query import BooksQuery
-# from shop.tests.factories.category import CategoryFactory
-# from shop.tests.factories.book import BookFactory
+from django.test import TestCase
+from shop.queries.books_query import BooksQuery
+from shop.tests.factories.category import CategoryFactory
+from shop.tests.factories.book import BookFactory
 
-# class TestBooksService(TestCase):
-#     def setUp(self):
-#         self.category_1 = CategoryFactory.create()
-#         self.category_2 = CategoryFactory.create()
-#         self.books_1 = [BookFactory.create(category = self.category_1, name = 'AAA') for i in range(3)]
-#         self.books_2 = [BookFactory.create(category = self.category_2, name = 'BBB') for i in range(3)]
+class TestBooksService(TestCase):
+    def setUp(self):
+        self.category_1 = CategoryFactory.create()
+        self.category_2 = CategoryFactory.create()
+        self.books_1 = [BookFactory.create(category = self.category_1, name = 'AAA') for i in range(3)]
+        self.books_2 = [BookFactory.create(category = self.category_2, name = 'BBB') for i in range(3)]
    
-#     def test_call_empty_sorter(self):
-#         books_query = BooksQuery(filter = self.category_1.id, sorter = '')
+    def test_call_empty_sorter(self):
+        books_query = BooksQuery(filter = self.category_1.id, sorter = '')
 
-#         for book in books_query.call():
-#             self.assertIn(book, self.books_1)
-#         self.assertEqual(books_query.call()[0].name, 'AAA')# default sorter => by title
+        for book in books_query.call():
+            self.assertIn(book, self.books_1)
+        self.assertEqual(books_query.call()[0].name, 'AAA')# default sorter => by title
     
-#     def test_call_with_sorter(self):
-#         books_query = BooksQuery(filter = self.category_1.id, sorter = '-price')
+    def test_call_with_sorter(self):
+        books_query = BooksQuery(filter = self.category_1.id, sorter = '-price')
 
 
-#         for book in books_query.call():
-#             self.assertIn(book, self.books_1)
+        for book in books_query.call():
+            self.assertIn(book, self.books_1)
         
-#         self.assertGreater(books_query.call()[0].price, books_query.call()[1].price )# sorting py price hight to low
+        self.assertGreater(books_query.call()[0].price, books_query.call()[1].price )# sorting py price hight to low
--- shop/tests/test_models.py
+++ shop/tests/test_models.py
@@ -1,17 +1,17 @@
-# from django.test import TestCase
-# from shop.models import Book
-# from shop.tests.factories.author import AuthorFactory
-# from shop.tests.factories.book import BookFactory
+from django.test import TestCase
+from shop.models import Book
+from shop.tests.factories.author import AuthorFactory
+from shop.tests.factories.book import BookFactory
 
-# class TestModels(TestCase):
-#     def setUp(self):
-#         self.authors = [AuthorFactory.create(), AuthorFactory.create()]
-#         self.book = BookFactory.create(authors = self.authors)
+class TestModels(TestCase):
+    def setUp(self):
+        self.authors = [AuthorFactory.create(), AuthorFactory.create()]
+        self.book = BookFactory.create(authors = self.authors)
 
-#     def test_book_all_authors(self):
-#         """
-#             Model Book, has custom method all_authors =>(self.authors)=> 'take all author for current_book and return: first_name last_name, first_name last_name ...'
-#         """
+    def test_book_all_authors(self):
+        """
+            Model Book, has custom method all_authors =>(self.authors)=> 'take all author for current_book and return: first_name last_name, first_name last_name ...'
+        """
 
-#         result = ', '.join(author.first_name + ' ' + author.last_name for author in self.authors)
-#         self.assertEqual(self.book.all_authors(), result)
+        result = ', '.join(author.first_name + ' ' + author.last_name for author in self.authors)
+        self.assertEqual(self.book.all_authors(), result)
