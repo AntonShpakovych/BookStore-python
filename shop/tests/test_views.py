8d56b8fec2475c1e785aecc170724d668e17f739
Anton Shpakovych <shpakovych18@gmail.com>
2022-11-07 13:13:32 +0200

    fixed test

--- shop/tests/test_views.py
+++ shop/tests/test_views.py
@@ -11,7 +11,7 @@ class TestViews(TestCase):
         response = self.client.get(reverse('home'))
 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'shop/pages/home.html')
+        self.assertTemplateUsed(response, 'pages/home.html')
     
     def test_books_page_get(self):
         response = self.client.get(reverse('books'))
